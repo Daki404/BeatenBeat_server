f0fbbd579520a2c97a29091a99afd19bd0a0190e
Jangjun <daki403.1@gmail.com>
2024-02-25 14:54:17 +0900

    build(gradle & yml) : split profile local | prod
    로컬 개발과 배포시에, 옵션이 달라지는 부분이 있어
    profile을 구분 하였습니다.
    
    ./gradlew build -Pprofile=prod / 같은 방식으로
    사용하며, default는 local 입니다.

--- src/main/java/daki/BeatenBeat/controller/MainController.java
+++ src/main/java/daki/BeatenBeat/controller/MainController.java
@@ -1,6 +1,7 @@
 package daki.BeatenBeat.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 public class MainController {
 
+    @Value("${domain}")
+    private String domain;
+
     @GetMapping("/")
     public String redirectMain() {
-        return "redirect:http://beaten-beat.com";
+        return "redirect:http://" + domain;
     }
 }
